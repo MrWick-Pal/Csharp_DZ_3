63727ff308e1a10379536abab8a843667d6490d7
Matrix <cfifgfkm@gmail.com>
2022-12-02 22:52:05 +0300

    DZ fix

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,16 +4,16 @@ double x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Y: ");
 double y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату X2: ");
+Console.WriteLine("Введите координату Z: ");
 double z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y2: ");
+Console.WriteLine("Введите координату X2: ");
 double x2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату X3: ");
+Console.WriteLine("Введите координату Y2: ");
 double y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y3: ");
+Console.WriteLine("Введите координату Z2: ");
 double z2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
