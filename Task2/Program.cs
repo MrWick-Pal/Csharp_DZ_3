8e9807dd4f9eb33306f264dfc617831889e4b989
Matrix <cfifgfkm@gmail.com>
2022-12-02 22:48:36 +0300

    DZ done.

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координату X: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X2: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X3: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y3: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
---
+++ Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i} {i * i * i}");
+}
